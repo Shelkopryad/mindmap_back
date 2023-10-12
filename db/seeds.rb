1365082046fe37ae49117640f3a3a0f567ed02bd
Shelkopryad <welkopr9d@gmail.com>
2023-10-12 18:45:47 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CheckItem.new(item_to_check: 'заказ открывается в МА и CRM и показывает одинаковые данные', to_test: false).save
+CheckItem.new(item_to_check: 'экспорт данных по заказам', to_test: false).save
+CheckItem.new(item_to_check: 'изменение цены у продукта отображается при добавлении его в заказ', to_test: false).save
+CheckItem.new(item_to_check: 'продукт создаётся и виден как в МА так и в CRM', to_test: false).save
+
+Tag.new(tag: 'product').save
+Tag.new(tag: 'order').save
+
+CheckItemTag.new(check_item_id: 1, tag_id: 2).save
+CheckItemTag.new(check_item_id: 2, tag_id: 2).save
+CheckItemTag.new(check_item_id: 3, tag_id: 2).save
+CheckItemTag.new(check_item_id: 3, tag_id: 1).save
+CheckItemTag.new(check_item_id: 4, tag_id: 1).save
+
